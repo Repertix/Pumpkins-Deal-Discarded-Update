bb0e99eae914ec7f13a239e3154040092473a98a
Reper <43357489+Repertix@users.noreply.github.com>
2021-07-13 16:06:48 -0500

    Update levelTwo.gd

--- scenes/levelTwo.gd
+++ scenes/levelTwo.gd
@@ -1,6 +1,9 @@
 extends Node2D
 var trucks = 0
 signal _callUI(type)
+#Funny fact: For sprites, I had to play a duplicated version of Yandere Simulator, to get some references from Japanese school
+#Another funny fact: Roblox uses a lot the GPU, and I amazed how it works more smooth that the original Yandere Simulator
+#You're rlly dumb, Alex.
 func _ready():
 	GameManager.recentMap = "res://scenes/School.tscn"
 
